ae07537043fc1f974c019f812bb8d269a47c5f3c
Thomas <thomas.suignard.ts@gmail.com>
2017-12-02 12:04:23 +0400

    ajout de spring jpa pour toutes les tables

--- simulator/src/main/java/kobdig/repository/SauvegardeRepository.java
+++ simulator/src/main/java/kobdig/repository/SauvegardeRepository.java
@@ -1,8 +1,10 @@
-package kobdig.sauvegarde;
+package kobdig.repository;
 
+import kobdig.tables.Sauvegarde;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-@Component
+@Repository
 public interface SauvegardeRepository extends CrudRepository<Sauvegarde, Integer> {
 }
