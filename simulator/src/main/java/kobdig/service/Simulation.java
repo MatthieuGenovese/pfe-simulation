04b7612f64d2bcf57f6477899f5bee184c46492a
Thomas <thomas.suignard.ts@gmail.com>
2018-02-06 09:26:50 +0100

    corr

--- simulator/src/main/java/kobdig/service/Simulation.java
+++ simulator/src/main/java/kobdig/service/Simulation.java
@@ -1,5 +1,6 @@
 package kobdig.service;
 
+import kobdig.communication.Sender;
 import kobdig.mongo.collections.*;
 import kobdig.mongo.repository.*;
 import kobdig.agent.Agent;
@@ -56,6 +57,9 @@ public class Simulation {
 
     @Autowired
     private HouseholdMongoRepository householdMongoRepository;
+
+    @Autowired
+    private Sender sender;
 
     /** Execution delay in milliseconds */
     private volatile int executionDelay = 10;
@@ -204,6 +208,7 @@ public class Simulation {
         if (builder.getTime() >= builder.getNumSim()) {
             System.err.println("SIMULATION FINISHED");
             running = false;
+            sender.send("extract", Integer.toString(builder.getId()));
         } else {
             System.err.println("STEP " + (builder.getTime()+1) + "/" + builder.getNumSim());
             int occuped = 0;
